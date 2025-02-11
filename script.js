8ab304434f8746cb48631b1bcd6c69e2cc0dcaaa
Fena551 <kakadiyafena@gmail.com>
2025-02-11 10:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
+//function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+//function SportsCar(make, model, topSpeed) {}
 
+
+class Car{
+	constructor(make,model){
+		this.make=make;
+		this,model=model;
+	}
+}
+
+Car.prototype.getMakeModel=function(){
+	return `${this.make} ${this.model)`;
+}
+
+class SportsCar extends Car{
+	constructor(make,model,topSpeed){
+		super(make,model);
+		this.topSpeed=topSpeed;
+	}
+}
+
+
+Sportcar.prototype.getTopSpeed=function(){
+	return this.topspeed;
+}
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
